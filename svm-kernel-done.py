9d47d88ebb43e777d519d1de7226c55cf4794f88
Mahardika23 <aryagamas@gmail.com>
2020-05-20 22:33:09 +0700

    Remove unused functions

--- svm-kernel-done.py
+++ svm-kernel-done.py
@@ -117,6 +117,60 @@ class SVM(object):
 if __name__ == "__main__":
     import pylab as pl
 
+    def gen_lin_separable_data():
+        # generate training data in the 2-d case
+        mean1 = np.array([0, 2])
+        mean2 = np.array([2, 0])
+        cov = np.array([[0.8, 0.6], [0.6, 0.8]])
+        X1 = np.random.multivariate_normal(mean1, cov, 100)
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 100)
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def gen_non_lin_separable_data():
+        mean1 = [-1, 2]
+        mean2 = [1, -1]
+        mean3 = [4, -4]
+        mean4 = [-4, 4]
+        cov = [[1.0, 0.8], [0.8, 1.0]]
+        X1 = np.random.multivariate_normal(mean1, cov, 50)
+        X1 = np.vstack((X1, np.random.multivariate_normal(mean3, cov, 50)))
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 50)
+        X2 = np.vstack((X2, np.random.multivariate_normal(mean4, cov, 50)))
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def gen_lin_separable_overlap_data():
+        # generate training data in the 2-d case
+        mean1 = np.array([0, 2])
+        mean2 = np.array([2, 0])
+        cov = np.array([[1.5, 1.0], [1.0, 1.5]])
+        X1 = np.random.multivariate_normal(mean1, cov, 100)
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 100)
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def split_train(X1, y1, X2, y2):
+        X1_train = X1[:90]
+        y1_train = y1[:90]
+        X2_train = X2[:90]
+        y2_train = y2[:90]
+        X_train = np.vstack((X1_train, X2_train))
+        y_train = np.hstack((y1_train, y2_train))
+        return X_train, y_train
+
+    def split_test(X1, y1, X2, y2):
+        X1_test = X1[90:]
+        y1_test = y1[90:]
+        X2_test = X2[90:]
+        y2_test = y2[90:]
+        X_test = np.vstack((X1_test, X2_test))
+        y_test = np.hstack((y1_test, y2_test))
+        return X_test, y_test
+
     def plot_margin(X1_train, X2_train, clf):
         def f(x, w, b, c=0):
             # given x, return y such that [x,y] in on the line
@@ -168,6 +222,51 @@ if __name__ == "__main__":
         pl.axis("tight")
         pl.show()
 
+    def test_linear():
+        X1, y1, X2, y2 = gen_lin_separable_data()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+        print(y_train)
+        clf = SVM()
+        clf.fit(X_train, y_train)
+
+        y_predict = clf.predict(X_test)
+        correct = np.sum(y_predict == y_test)
+        print("%d out of %d predictions correct" % (correct, len(y_predict)))
+
+        plot_margin(X_train[y_train == 1], X_train[y_train == -1], clf)
+
+    def test_non_linear():
+        X1, y1, X2, y2 = gen_non_lin_separable_data()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+        clf = SVM(polynomial_kernel)
+        clf.fit(X_train, y_train)
+
+        y_predict = clf.predict(X_test)
+        correct = np.sum(y_predict == y_test)
+        print("%d out of %d predictions correct" % (correct, len(y_predict)))
+
+        plot_contour(X_train[y_train == 1], X_train[y_train == -1], clf)
+
+    def test_soft():
+        X1, y1, X2, y2 = gen_lin_separable_overlap_data()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+        clf = SVM(C=1000.1)
+        clf.fit(X_train, y_train)
+
+        y_predict = clf.predict(X_test)
+        correct = np.sum(y_predict == y_test)
+        print("%d out of %d predictions correct" % (correct, len(y_predict)))
+
+        plot_contour(X_train[y_train == 1], X_train[y_train == -1], clf)
+
+    # test_linear()
+    # test_non_linear()
+    # test_soft()
 
 # %%
 iris = datasets.load_iris()
@@ -300,26 +399,8 @@ def DAG(X, testData):
         print("HASIL AKHIR METODE DAG :", clf_2(testData))
     # =============================================================
 
-    # CLASSIFIER 2
 
-
-# X = iris.data
-# print(X[:, 0])
-# f = plt.figure()
-# s = plt.scatter(X[:, 0], X[:, 1], c='y')
-# plt.show(s)
 # Jalanin Satu Aja Kalo bingung
-# oneversusrest(X, X[101])
-# DAG(X, X[101])
-def wrapper(func, *args, **kwargs):
-    def wrapped():
-        return func(*args, **kwargs)
-    return wrapped
+oneversusrest(X, X[101])
+DAG(X, X[101])
 
-
-short_list = range(10)
-wrapped = wrapper(oneversusrest, X, X[101])
-# print(timeit.timeit(wrapped, number=1))
-wrapped = wrapper(oneversusrest, X, X[101])
-
-print("Function took: ", timeit.timeit(wrapped, number=1))
